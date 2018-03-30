ae9f1809c422bcf57bff77cb02d4ea6b7f9034b3
yantao <tao.yan@picahealth.com>
2018-03-30 09:36:00 +0800

    新增，观察spring 如何初始化IOC的。

--- spring-test/springIOCTest/src/test/gy/life/cycle/BeanLifeCycleTest.java
+++ spring-test/springIOCTest/src/test/gy/life/cycle/BeanLifeCycleTest.java
@@ -16,7 +16,9 @@ public class BeanLifeCycleTest {
     public void testLifeCycle() {
         ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("life-cycle-all.xml");
 //        ApplicationContext ctx = new ClassPathXmlApplicationContext("life-cycle.xml");
+        System.out.println("4-1 容器初始化成功");
         ctx.getBean("cycle", BeanLifeCycle.class);
+        System.out.println("现在开始关闭容器！");
         ctx.registerShutdownHook();
 
     }
